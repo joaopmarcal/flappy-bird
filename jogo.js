d87ad959d66500a56bd8e1cce5121915f42abd5e
Joao Paulo <joaopaulozangrande@gmail.com>
2022-05-04 18:37:33 -0300

    I drew the flappy bird in the screen

--- jogo.js
+++ jogo.js
@@ -5,3 +5,29 @@ sprites.src = './sprites.png';
 
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
+
+const flappyBird = {
+  spriteX: 0,
+  spriteY: 0,
+  largura: 33,
+  altura: 24,
+  x: 10,
+  y: 50,
+  desenha() {
+    contexto.drawImage(
+      sprites,
+      flappyBird.spriteX,flappyBird.spriteY,
+      flappyBird.largura,flappyBird.altura,
+      flappyBird.x,flappyBird.y,
+      flappyBird.largura,flappyBird.altura,
+    );
+  }
+}
+
+function loop() {
+
+  flappyBird.desenha();
+  requestAnimationFrame(loop);
+}
+
+loop();
